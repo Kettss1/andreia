d6e7805e6f632203e5435aa9b87d6595ab0267ee
Kettss <ketlin.kunden@gmail.com>
2022-05-30 11:39:18 -0300

    created slug mixin

--- services/andreia-backend/andreia/model/__init__.py
+++ services/andreia-backend/andreia/model/__init__.py
@@ -1,49 +1,61 @@
+from decimal import Decimal
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+from andreia.model.base import PrimaryKeyMixin, SlugMixin
+
 Base = declarative_base()
 
 
-class User(Base):
-    __table_name__ = 'user'
+class User(Base, PrimaryKeyMixin):
+    name = Column(String, nullable=False, unique=True)
+    # Login with email, so, e-mail must be unique
+    email = Column(String, nullable=False, unique=True)
 
-    id = Column('id', Integer, primary_key=True)
-    name = Column('name', String, nullable=False, unique=True)
-    password = Column('password', String, nullable=False)
-    email = Column('email', String, nullable=False)
+    # TODO: Estudar porque https://stackoverflow.com/a/9595108
+    #  exemplo implementacao em scratch.py
+    #  testar esse fluxo de
+    #  - desenvolver pequenas POCs em SCRATCH_XX.PY
+    #  - brincar com o python live no botao Python Console (ao lado de TOD0)
+    password = Column(String, nullable=False)
+    password_salt = Column(String, nullable=False)
     restaurants = relationship('Restaurant')
 
+    # TODO: Implementar funcoes:
+    #  - Setpassword
+    #  - Checkpassword
+    #  Ambas recebem uma string e settam os campos adequados, OU comparam (fazem o calculo reverso)
 
-class Restaurant(Base):
-    __table_name__ = 'restaurant'
 
-    id = Column('id', Integer, primary_key=True)
-    name = Column('name', String, nullable=False, unique=True)
-    description = Column('description', String)
-    delivery_charge = Column('delivery_charge', Integer)
+class Restaurant(Base, PrimaryKeyMixin, SlugMixin):
+    user_id = Column(PrimaryKeyMixin.id.type, ForeignKey('user.id'), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    delivery_charge = Column(Decimal)
     menus = relationship('Menu')
 
 
-class Menu(Base):
-    __table_name__ = 'menu'
+class Menu(Base, PrimaryKeyMixin, SlugMixin):
+    name = Column(String, nullable=False)
+    restaurant_id = Column(PrimaryKeyMixin.id.type, ForeignKey('restaurant.id'), nullable=False)
+    restaurant: Restaurant = relationship(Restaurant)
+    sections = relationship('MenuSection')
 
-    id = Column('id', Integer, primary_key=True)
+
+class MenuSection(Base, PrimaryKeyMixin, SlugMixin):
+    menu_id = Column(PrimaryKeyMixin.id.type, ForeignKey('menu.id'), nullable=False)
+    menu: Menu = relationship(Menu)
+    name = Column(String, nullable=False)
+    # Section position
+    index = Column(Integer, nullable=False)
     items = relationship('MenuItem')
 
 
-class MenuItem(Base):
-    __table_name__ = 'menu_item'
-
-    id = Column('id', Integer, primary_key=True)
-    name = Column('name', String, nullable=False, unique=True)
-    price = Column('price', Integer, nullable=False)
-    category_id = Column(Integer, ForeignKey("category.id"))
-
-
-class Category(Base):
-    __table_name__ = 'category'
-
-    id = Column('id', Integer, primary_key=True)
-    name = Column('name', String, nullable=False)
-    slug = Column('slug', String, nullable=False, unique=True)
+class MenuItem(Base, PrimaryKeyMixin, SlugMixin):
+    section = relationship('MenuSection')
+    name = Column(String, nullable=False, unique=True)
+    price = Column(Decimal, nullable=False)
+    section_id = Column(PrimaryKeyMixin.id.type, ForeignKey("menusection.id"))
+    index = Column(Integer, nullable=False)
---
+++ services/andreia-backend/andreia/model/base.py
@@ -0,0 +1,18 @@
+import uuid
+
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declared_attr
+
+
+class PrimaryKeyMixin:
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: uuid.UUID = Column('id', UUID, primary_key=True)
+
+
+class SlugMixin:
+    # TODO: Add random string to the end so that you never hit uniqueness constraints
+    slug = Column(String, nullable=False, unique=True)
